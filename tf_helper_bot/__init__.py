3fb28ba1eeda8b83607af42ca39f463a4579dfe7
Ceshine Lee <shuanck@gmail.com>
2020-01-03 15:26:10 +0800

    Add a scheduler

--- tf_helper_bot/__init__.py
+++ tf_helper_bot/__init__.py
@@ -2,3 +2,4 @@ from .bot import BaseBot, BaseDistributedBot
 from .callbacks import *
 from .logger import Logger
 from .metrics import (Metric, FBeta, AUC)
+from .lr_schedulers import CosineDecayWithWarmup
