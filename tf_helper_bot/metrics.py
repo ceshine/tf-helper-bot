de97b6a365c961e7498c05e199c647ec2b667c93
Ceshine Lee <shuanck@gmail.com>
2020-03-26 21:17:36 +0800

    More metrics

--- tf_helper_bot/metrics.py
+++ tf_helper_bot/metrics.py
@@ -3,7 +3,7 @@ from typing import Tuple, Union
 
 import numpy as np
 # import tensorflow as tf
-from sklearn.metrics import fbeta_score, roc_auc_score
+from sklearn.metrics import fbeta_score, roc_auc_score, f1_score, confusion_matrix
 from sklearn.exceptions import UndefinedMetricWarning
 
 
@@ -63,3 +63,34 @@ class AUC(Metric):
         auc_score = roc_auc_score(
             truth.astype("int"), pred)
         return auc_score * -1, f"{auc_score * 100:.2f}"
+
+
+class MultiClassF1Score(Metric):
+    name = "f1"
+
+    def __init__(self, verbose: bool = False):
+        super().__init__()
+        self.verbose = verbose
+
+    def __call__(self, truth: np.ndarray, pred: np.ndarray) -> Tuple[float, str]:
+        preds = np.argmax(pred, axis=-1).reshape(-1)
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', category=UndefinedMetricWarning)
+            scores = f1_score(truth, preds, average=None)
+        if self.verbose:
+            print(", ".join([f"{x:.4f}" for x in scores]))
+            print(confusion_matrix(truth, preds))
+        avg_scores = np.mean(scores)
+        return avg_scores * -1, f"{avg_scores * 100:.2f}%"
+
+
+class Top1Accuracy(Metric):
+    """Accuracy for Multi-class classification"""
+    name = "accuracy"
+
+    def __call__(self, truth: np.ndarray, pred: np.ndarray) -> Tuple[float, str]:
+        correct = np.sum(
+            truth.reshape(-1) == np.argmax(pred, axis=-1).reshape(-1)).item()
+        total = truth.reshape(-1).shape[0]
+        accuracy = (correct / total)
+        return accuracy * -1, f"{accuracy * 100:.2f}%"
